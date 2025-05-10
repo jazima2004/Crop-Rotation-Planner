589f20f0a0a9003e49de5b298e5a59dd2a6f8f73
jazima2004 <jazimaa83@gmail.com>
2025-05-10 14:52:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ st.markdown(
     """
     <style>
     .stApp {
-        background-image: url("https://raw.githubusercontent.com/jazima2004/Crop-Rotation-Planner/main/agri1.jpg");
+        background-image: url("https://raw.githubusercontent.com/jazima2004/Crop-Rotation-Planner/main/Premium-Photo-_-Empty-wood-table-top-and-blurred-green-tree.jpg");
         background-size: cover;
         background-position: center;
         background-repeat: no-repeat;
